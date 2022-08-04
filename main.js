55318d053143f0672366fde8c20566909521071d
Igor Sergeev <igor@tomatstudio.ru>
2022-08-04 13:01:03 +0300

    attrChange

--- main.js
+++ main.js
@@ -4,18 +4,39 @@
 const template = document.createElement('template');
 template.innerHTML = `
   <div class="card">
-    <h4>Это карточка новости</h4>
+    <h4>hello</h4>
   </div>
 `;
 class NewsCard extends HTMLElement {
+
   constructor() {
     super();
+    this._title = null;
+    this._rendered = false;
   }
 
+  static observedAttributes = ['title'];
+
+  render() {
+    console.log('rendering');
+    this._title = this.getAttribute('title') || '_заголовок_новости_';
+    this.innerHTML = `<h4>${this._title}</h4>`;
+  }  
+
   connectedCallback() {
-    //this.innerHTML = this.getAttribute('title');
-    this.innerHTML = template;
+    console.log('connectedCallback..');
+    if (!this._rendered) {
+      this.render();
+      this._rendered = true;
+    }   
+  }
+
+
+  attributeChangedCallback(attrName, oldValue, newValue) {
+    console.log('attrChange..');
+    this.render();
   }
 };
+
 
 customElements.define('news-card', NewsCard);
