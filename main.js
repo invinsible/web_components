0788e9c0f8dbd38a0573a44b8654eb11dce05d09
Igor Sergeev <igor@tomatstudio.ru>
2022-08-04 12:10:23 +0300

    first commit for learnJs

--- main.js
+++ main.js
@@ -1,49 +1,21 @@
+// автономные пользовательские элементы наследуем от HTMLElement
+// пользовательские встроенные элементы наследуем от класса элемента, например HTMLButtonElement 
+
 const template = document.createElement('template');
 template.innerHTML = `
-  <style>
-    h4 {
-      margin: 0;
-      color: blue;
-    }
-    p {
-      margin: 0;
-    }
-  </style>
-  <h4></h4>
-  <div class="contacts">
-    <p><slot name="phone"></slot></p>
-    <p><slot name="email"></slot></p>    
+  <div class="card">
+    <h4>Это карточка новости</h4>
   </div>
-  <button id="hideBtn">Скрыть контакты</button>
 `;
-
-class CustomQuiz extends HTMLElement {
+class NewsCard extends HTMLElement {
   constructor() {
     super();
-    this.showInfo = true;
-    this.attachShadow({mode: 'open'});
-    this.shadowRoot.appendChild(template.content.cloneNode(true));
-    let shadowH4 = this.shadowRoot.querySelector('h4');
-    shadowH4.innerText = `${this.getAttribute('title') || 'Опрос'}`;    
-  }
-
-  toggleShow() {
-    const contacts = this.shadowRoot.querySelector('.contacts');
-    const btn = this.shadowRoot.querySelector('#hideBtn');
-    this.showInfo = !this.showInfo;
-    
-    if (this.showInfo) {
-      contacts.style.display = 'block';
-      btn.innerText = 'Скрыть контакты';
-    } else {
-      contacts.style.display = 'none';
-      btn.innerText = 'Показать контакты';
-    }
   }
 
   connectedCallback() {
-    this.shadowRoot.querySelector('#hideBtn').addEventListener('click', () => this.toggleShow());
+    //this.innerHTML = this.getAttribute('title');
+    this.innerHTML = template;
   }
 };
 
-customElements.define('custom-quiz', CustomQuiz);
+customElements.define('news-card', NewsCard);
